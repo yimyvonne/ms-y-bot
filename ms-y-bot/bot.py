827820a8ee0a7d5147cd1b13a4dfff3adbc89036
Yvonne Yim <yimiw.yvonne@gmail.com>
2021-12-31 18:53:09 +0000

    determine if follow request should be sent

--- ms-y-bot/bot.py
+++ ms-y-bot/bot.py
@@ -3,6 +3,7 @@ import random
 
 import numpy as numpy
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
@@ -16,7 +17,6 @@ def wait(minWaitTime, maxWatitime, operation_name):
 class Bot:
     def __init__(self, username, password):
         self.driver = webdriver.Chrome("chromedriver")
-        self.logged_in = False
         self.username = username
         self.password = password
         self.base_url = "https://www.instagram.com"
@@ -24,7 +24,7 @@ class Bot:
     def go_to_user(self, user):
         # Goes to a user's page
         self.driver.get("{}/{}/".format(self.base_url, user))
-        wait(4, 5, "opened user profile: ", user)
+        wait(4, 5, "opened user profile: {}".format(user))
 
     def set_value_to_input_by_name(self, name, value):
         input = self.driver.find_element(By.NAME, name)
@@ -44,8 +44,11 @@ class Bot:
         self.set_value_to_input_by_name("password", self.password)
         wait(1, 2, "set password")
         print("Waiting for login form to be clickable")
-        WebDriverWait(self.driver, 20).until(expected_conditions.element_to_be_clickable((By.ID, "loginForm"))).click()
-        self.logged_in = True
+        try:
+            WebDriverWait(self.driver, 20).until(expected_conditions.element_to_be_clickable((By.ID, "loginForm"))).click()
+        except TimeoutException:
+            print("likely blocked by insta temporarily")
+
 
         # WebDriverWait(self.driver, 20).until(expected_conditions.element_to_be_clickable(
         #     (By.XPATH, "//button[contains(text(), 'Allow All Cookies')]"))).click()
@@ -69,7 +72,7 @@ class Bot:
         WebDriverWait(self.driver, 20).until(
             expected_conditions.element_to_be_clickable((By.XPATH, "//a[contains(@href,'/followers')]"))).click()
         wait(2, 3, "clicked on followers pop up")
-        scroll_box = self.driver.find_element(By.XPATH, "/html/body/div[6]/div/div/div[2]")
+        scroll_box = self.driver.find_element(By.CLASS_NAME, "isgrP")
         last_ht, ht = 0, 1
         while last_ht != ht:
             last_ht = ht
@@ -83,26 +86,39 @@ class Bot:
         print("account scrapped: {} ".format(names))
         return names
 
+    def find_user_profile_element(self, by, identifier):
+        try:
+            return self.driver.find_element(by, identifier)
+        except NoSuchElementException:
+            return False
+
     def follow_followers(self, follower_list):
         followed = []
         for username in follower_list:
             self.go_to_user(username)
-            follow_button = self.driver.find_element(By.CSS_SELECTOR, 'button')
-            button_text = follow_button.text.strip()
-            print("button text = ", button_text)
-            if button_text == "Follow":
-                if self.driver.find_element(By.XPATH,
-                                            " //*[ contains (text(), ‘This Account is Private’)]").isDisplayed():
-                    print("private account: skipping")
-                    break
-                else:
-                    follow_button.click()
-                    followed.append(username)
-                    wait(1, 10, "requested to follow {}".format(username))
-            elif button_text == "Requested":
+            # follow_button = self.driver.find_element(By.CSS_SELECTOR, 'button')
+            # button_text = follow_button.text.strip()
+            # print("button text = ", button_text)
+
+            if self.find_user_profile_element(By.XPATH, "//*[contains(text(), 'Requested')]"):
                 print("skipping: already requested.")
-            elif button_text == "Follow Back":
+                continue
+
+            if self.find_user_profile_element(By.XPATH, "//*[contains(text(), 'Follow Back')]"):
                 print("skipping: already following you.")
+                continue
+
+            try:
+                self.driver.find_element(By.XPATH, "//*[contains(text(), 'This account is private')]")
+                self.driver.find_element(By.XPATH, "(//button[contains(text(),'Follow')])").click()
+                followed.append(username)
+                wait(1, 60, "requested to follow {}".format(username))
+            except NoSuchElementException:
+                if self.find_user_profile_element(By.CSS_SELECTOR, "[aria-label='Following']"):
+                    print("skipping: you are already following this account")
+                else:
+                    print("skipping: public account")
+                pass
 
         print("requested to follow the following accounts: ", followed)
 
@@ -114,11 +130,11 @@ class Bot:
 # (4) store account credential separately
 
 def main():
-    my_bot = Bot("******", "*****+")
+    my_bot = Bot("*", "*")
+    target_account = input("Which account do you want to scrape followers from? ")
     my_bot.login()
-    target_followers = my_bot.get_followers("bno_working_potatoes")
+    target_followers = my_bot.get_followers(target_account)
     my_bot.follow_followers(target_followers)
-
 
 if __name__ == "__main__":
     main()
